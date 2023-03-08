957309e426eb934ce2e9f4d4d6e89f1f903f1646
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-03-09 01:00:30 +0200

    hardcode message targetOrigin

--- projects/DigitPop-Player/src/app/video/video.component.ts
+++ projects/DigitPop-Player/src/app/video/video.component.ts
@@ -135,7 +135,7 @@ export class VideoComponent implements OnInit, AfterViewInit {
       // TODO: change targetOrigin url for staging/live deployment
       window.parent.postMessage({
         init: true, action: 'getCampaignId'
-      }, 'https://digitpop.com');
+      }, 'http://digitpop.com');
 
       addEventListener('message', (event) => {
         if (event.data.campaignId) {
@@ -175,7 +175,7 @@ export class VideoComponent implements OnInit, AfterViewInit {
 
   onStartVideo() {
     const targetWindow = window.parent;
-    targetWindow.postMessage('start', environment.homeUrl);
+    targetWindow.postMessage('start', 'http://digitpop.com');
     if (!this.preview && this.subscription != null) {
       this.adService.createView(this.adId, this.subscription.cycleId).subscribe((res) => {
         console.log(res);
@@ -247,7 +247,7 @@ export class VideoComponent implements OnInit, AfterViewInit {
     this.showCanvas = false;
 
     // TODO: Change the URL
-    targetWindow.postMessage('exit', environment.homeUrl);
+    targetWindow.postMessage('exit', 'http://digitpop.com');
     // targetWindow.postMessage('exit', `${environment.homeUrl}`);
   }
 
