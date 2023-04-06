d9ab0353f3bd62cdb38a14b8f52831d89e61ae94
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-04-06 22:49:40 +0200

    Improve email component

--- projects/DigitPop-CMS/src/app/email-verification/email-verification.component.ts
+++ projects/DigitPop-CMS/src/app/email-verification/email-verification.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../xchane/services/data.service';
+import {
+  XchaneAuthenticationService
+} from '../shared/services/xchane-auth-service.service';
+import {LoginComponent} from '../login/login.component';
+import {MatDialog} from '@angular/material/dialog';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'digit-pop-email-verification',
@@ -7,11 +13,27 @@ import {DataService} from '../xchane/services/data.service';
   styleUrls: ['./email-verification.component.scss']
 })
 export class EmailVerificationComponent implements OnInit {
+  isLoggedIn = true;
 
-  constructor(private data: DataService) {
+  constructor(private data: DataService, private auth: XchaneAuthenticationService, public dialog: MatDialog, private router: Router) {
+    if (auth.currentUserValue._id) {
+      this.isLoggedIn = true;
+    }
   }
 
   ngOnInit(): void {
+  }
+
+  openLoginDialog(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      panelClass: 'dpop-modal'
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      if (this.auth.currentUserValue) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 
 }
