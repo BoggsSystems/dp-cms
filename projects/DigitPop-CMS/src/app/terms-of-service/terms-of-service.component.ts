6ab5af9db5adcbf4a8bc2eb761fff84206c4c971
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-05-06 18:50:03 +0300

    Refactor terms list & scroll to section

--- projects/DigitPop-CMS/src/app/terms-of-service/terms-of-service.component.ts
+++ projects/DigitPop-CMS/src/app/terms-of-service/terms-of-service.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './terms-of-service.component.html',
   styleUrls: ['./terms-of-service.component.scss']
 })
+
 export class TermsOfServiceComponent implements OnInit {
+
+  sections = [
+    { title: 'Lorem ipsum', id: 'link1' },
+    { title: 'Neque porro quisquam est qui', id: 'link2' },
+    { title: 'Lorem ipsum dolorem', id: 'link3' },
+    { title: 'adipiscing elit ipsum', id: 'link4' },
+    { title: 'Lorem ipsum', id: 'link5' }
+  ];
+  currentSectionId = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  scrollToSection(event: Event, id: string) {
+    event.preventDefault();
+    this.setCurrentSection(id);
+    const el = document.getElementById(id);
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const elTop = el.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+    window.scrollTo({ top: elTop, behavior: 'smooth' });
+  }
+
+  setCurrentSection(id: string) {
+    this.currentSectionId = id;
   }
 
 }
