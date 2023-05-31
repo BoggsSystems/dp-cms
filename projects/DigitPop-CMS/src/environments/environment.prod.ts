9b94bf48cc45c51f3f5cf75ee693ef342c6c7669
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-06-01 00:12:15 +0300

    Change and shorten player URL

--- projects/DigitPop-CMS/src/environments/environment.prod.ts
+++ projects/DigitPop-CMS/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://production-digitpop-server.herokuapp.com',
-  playerUrl: 'https://production-digitpop-player.herokuapp.com',
+  playerUrl: 'https://player.digitpop.com',
   // billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/Digitpop',
   // billsbyKey: 'digitpop_f1efba4792104a3ab66c1cb59f43993a',
   billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/stagingdigitpop',
